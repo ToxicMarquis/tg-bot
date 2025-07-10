261b07ab1b238aa5ecbbd8e8042e35fc6fb47cc2
Marquis <78649236+ToxicMarquis@users.noreply.github.com>
2025-07-10 19:44:54 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -15,16 +15,14 @@ class Config:
     DEVELOPER_ID = 1834341648
 
     # Настройки файлового хранилища
-    USERS_FILE = 'data/users.txt'
-    ACTIVITY_FILE = 'data/activity.txt'
-    TITLES_FILE = 'data/titles.json'
-    APP_DB_FILE = 'data/app_db.json'
+    USERS_FILE = '/data/users.txt'
+    ACTIVITY_FILE = '/data/activity.txt'
+    TITLES_FILE = '/data/titles.json'
     TOURNAMENTS_DIR = 'tournaments/'
 
-    # Настройки веб-сервера
+    # Настройки веб-сервера для UptimeRobot
     WEB_SERVER_HOST = '0.0.0.0'
     WEB_SERVER_PORT = 8080
-    WEB_APP_URL = os.environ.get('WEB_APP_URL', 'https://yourdomain.com/webapp')
 
     # Настройки логирования
     LOG_LEVEL = 'INFO'
@@ -36,10 +34,6 @@ class Config:
     # Настройки сообщений
     MAX_MESSAGE_LENGTH = 4000
 
-    # Настройки уровней
-    BASE_XP_REQUIREMENT = 50
-    XP_MULTIPLIER = 1.5
-
     @classmethod
     def validate(cls):
         """Валидация конфигурации"""
@@ -50,21 +44,3 @@ class Config:
             raise ValueError(f"Отсутствуют переменные окружения: {', '.join(missing_vars)}")
 
         return True
-
-    @classmethod
-    def calculate_xp_requirement(cls, level):
-        """Расчет требуемого опыта для уровня"""
-        return int(cls.BASE_XP_REQUIREMENT * (cls.XP_MULTIPLIER ** (level - 1)))
-
-    @classmethod
-    def calculate_level_from_xp(cls, total_xp):
-        """Расчет уровня по общему опыту"""
-        level = 1
-        accumulated_xp = 0
-        while accumulated_xp < total_xp:
-            xp_needed = cls.calculate_xp_requirement(level)
-            if accumulated_xp + xp_needed > total_xp:
-                break
-            accumulated_xp += xp_needed
-            level += 1
-        return level, accumulated_xp
--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask
 from threading import Thread
 import logging
-import os
 
-app = Flask(__name__, 
-            static_folder='webapp/static',
-            template_folder='webapp')
+app = Flask(__name__)
 logger = logging.getLogger(__name__)
 
 @app.route('/')
@@ -21,25 +18,9 @@ def status():
     return {
         "status": "running",
         "bot": "Chess Telegram Bot",
-        "version": "3.0.0",
+        "version": "2.0.0",
         "uptime": "online"
     }
-
-@app.route('/webapp')
-def webapp():
-    """Главная страница веб-приложения"""
-    return render_template('index.html')
-
-@app.route('/webapp/static/<path:filename>')
-def webapp_static(filename):
-    """Статические файлы для веб-приложения"""
-    return send_from_directory('webapp/static', filename)
-
-@app.route('/api/user/<username>')
-def get_user_data(username):
-    """API для получения данных пользователя"""
-    # Здесь будет логика загрузки данных пользователя
-    return jsonify({"username": username, "level": 1, "xp": 0})
 
 def run():
     """Запуск веб-сервера"""
